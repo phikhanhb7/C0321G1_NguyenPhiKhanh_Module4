840e2ef86672483e90b702c7a7b42b983068328f
phikhanhb7 <phikhanhb7@gmail.com>
2021-07-29 21:12:58 +0700

    up

--- 10_Session_Cookie/exercise/cart-item/src/main/java/com/example/controller/ShoppingCartController.java
+++ 10_Session_Cookie/exercise/cart-item/src/main/java/com/example/controller/ShoppingCartController.java
@@ -4,17 +4,11 @@ package com.example.controller;
 import com.example.model.entity.Cart;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ShoppingCartController {
-
-    @ModelAttribute("cart")
-    public Cart setupCart(){
-        return new Cart();
-    }
 
     @GetMapping("/cart")
     public ModelAndView showCart (@SessionAttribute("cart") Cart cart){
