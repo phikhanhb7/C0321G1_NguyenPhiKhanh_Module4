8650dc2e02fbd73b3428cc12881e2c681ec1a03c
phikhanhb7 <phikhanhb7@gmail.com>
2021-08-02 19:15:20 +0700

    up

---
+++ 13_i18n/practice/login-manager/src/main/java/com/example/LoginManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginManagerApplication.class, args);
+    }
+
+}
